cef55131d3216eb853efcd10d862ba067926e2b4
nttr <nttr@BI-23556.local>
2018-01-31 17:58:23 +0900

    指定時間にトースト

--- app/src/main/java/com/example/nttr/onealarm2/MainActivity.java
+++ app/src/main/java/com/example/nttr/onealarm2/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.nttr.onealarm2;
 
+import android.annotation.SuppressLint;
 import android.app.AlarmManager;
 import android.app.PendingIntent;
 import android.content.Context;
@@ -31,11 +32,10 @@ public class MainActivity extends AppCompatActivity {
         enter = (Button) findViewById(R.id.enter);
         enter.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v){
+            public void onClick(View v) {
 
                 timerSet();
             }
-
 
 
         });
@@ -46,60 +46,65 @@ public class MainActivity extends AppCompatActivity {
 //        }
 
 
-
-
     }
+
     //時を取得
-    private String mhours(){
-        Ehour=(EditText)this.findViewById(R.id.hour);
+    private String mhours() {
+        Ehour = (EditText) this.findViewById(R.id.hour);
         String hour = Ehour.getText().toString();
         return hour;
     }
+
     //分を取得
-    private String mminutes(){
-        Eminute=(EditText)this.findViewById(R.id.minute);
+    private String mminutes() {
+        Eminute = (EditText) this.findViewById(R.id.minute);
         String minute = Eminute.getText().toString();
         return minute;
     }
+
     //設定した時刻を表示
-    private void timeConf(String h, String m){
-        confview=(TextView)this.findViewById(R.id.confview);
+    private void timeConf(String h, String m) {
+        confview = (TextView) this.findViewById(R.id.confview);
         confview.setText("設定した時間は　" + h + "：" + m);
     }
 
 
-    public void timerSet(){
+    @SuppressLint("NewApi")
+    public void timerSet() {
         Intent intent = new Intent(getApplicationContext(), messageService.class);
         Context ct = getApplication();
         PendingIntent pendingIntent = PendingIntent.getService(ct, 0,
                 intent, PendingIntent.FLAG_UPDATE_CURRENT);
 
 
-
         Calendar calendar = Calendar.getInstance(TimeZone.getTimeZone("Asia/Tokyo")); // Calendar取得
         calendar.setTimeInMillis(System.currentTimeMillis()); // 現在時刻を取得
 
-
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy年MM月dd日hh時mm分ss秒");
         System.out.println(sdf.format(calendar.getTime()));
-//        calendar.set(Calendar.HOUR, Integer.parseInt(mhours()));
-//        calendar.set(Calendar.MINUTE, Integer.parseInt(mminutes()));
-//        calendar.set(Calendar.SECOND, 0);
-//        calendar.set(Calendar.MILLISECOND, 0);
 
-        calendar.add(Calendar.SECOND, 5);
+        calendar.set(Calendar.HOUR_OF_DAY, Integer.parseInt(mhours()));
+        calendar.set(Calendar.MINUTE, Integer.parseInt(mminutes()));
+        calendar.set(Calendar.SECOND, 0);
+        calendar.set(Calendar.MILLISECOND, 0);
+
+//        calendar.add(Calendar.SECOND, 5);
 
         System.out.println(sdf.format(calendar.getTime()));
 
-
-
-
-
         // AlarmManager の設定・開始
-        AlarmManager am = (AlarmManager)getSystemService(Context.ALARM_SERVICE);
-        am.set(AlarmManager.RTC_WAKEUP,calendar.getTimeInMillis(),pendingIntent);
+        AlarmManager am = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+//        API Level 19未満は、 set
+//        API Level 19以上 23未満は、 setExact
+//        API Level 23以上は、 setExactAndAllowWhileIdle
+        if (BuildConfig.VERSION_CODE < 19) {
+            am.set(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), pendingIntent);
+        } else if (BuildConfig.VERSION_CODE < 23) {
+            am.setExact(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), pendingIntent);
+        } else {
+            am.setExactAndAllowWhileIdle(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), pendingIntent);
+        }
     }
-
 
 
 }
